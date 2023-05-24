86fec5379bd2c41aa73a64790a1582c159082a52
Neal Nguyen <hluc2003@gmail.com>
2023-05-24 21:59:12 +0700

    update matrix square root

--- Neal/Matrix.cpp
+++ Neal/Matrix.cpp
@@ -4,6 +4,7 @@
 void Matrix()
 {
 	char chon;
+	int check;
 	while(chon!='5')
 	{
 		chon='0';
@@ -28,22 +29,38 @@ void Matrix()
 			{
 				do
 				{
+					check=0;
 					B1[j]=1;
 					B[j]=' ';
 					scanf("%lf",&A[i][j]);
 					scanf("%c",&B[j]);
-					if(B[j]=='/')
+					if(B[j]=='v'||B[j]=='V')
 					scanf("%lf",&B1[j]);
-					if(B[j]=='/')
+					if(B[j]=='v'||B[j]=='V')
 					{
-						if(B1[j]!=0)A[i][j]=A[i][j]/B1[j];
-						else 
+						if(B1[j]>0) A[i][j]=sqrt(B1[j]);
+						else
 						{
-							printf("The denominator must be not equal to zero!\n*Please enter the %dth row again: ",i+1);
+							printf("square root must be greater than zero!\n*Please enter the %dth row again: ",i+1);
 							fflush(stdin);
+							check=1;
 						}
 					}
-				} while(B1[j]==0);
+					else
+					{
+						if(B[j]=='/')
+						scanf("%lf",&B1[j]);
+						if(B[j]=='/')
+						{
+							if(B1[j]!=0)A[i][j]=A[i][j]/B1[j];
+							else 
+							{
+								printf("The denominator must be not equal to zero!\n*Please enter the %dth row again: ",i+1);
+								fflush(stdin);
+							}
+						}	
+					}
+				} while(B1[j]==0||check==1);
 			}
 			fflush(stdin);
 		}
