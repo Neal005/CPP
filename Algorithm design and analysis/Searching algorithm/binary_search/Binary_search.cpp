25807cbeae07427d7a7665295e02dc2105df5e15
Nguyễn Lê Hoàng Lực <120330789+Neal005@users.noreply.github.com>
2023-10-06 22:02:55 +0700

    Delete Algorithm design and analysis/Searching algorithm/binary_search directory

--- Algorithm design and analysis/Searching algorithm/binary_search/Binary_search.cpp
+++
@@ -1,8 +0,0 @@
-int binarysearch(int x,int *a,int left,int right)
-{
-	if(left>right) return -1;
-	int mid=(left + right)/2;
-	if(x==a[mid]) return mid;
-	if (x<a[mid]) return binarysearch(x,a,left,mid-1);
-	else return binarysearch(x,a,mid+1,right);
-}
