7abbcadb657d37220b702fb91caa3dc45bdf8b3e
Neal Nguyen <hluc2003@gmail.com>
2023-11-09 18:45:30 +0700

    Thay doi cau truc

--- Algorithm design and analysis/Algorithm design/Dynamic Partition algorithms/TheBackpack_DynamicPartition/TheBackpack1_DynamicPartition.cpp
+++ Algorithm design and analysis/Algorithm design/Dynamic Partition algorithms/TheBackpack_DynamicPartition/TheBackpack1_DynamicPartition.cpp
@@ -1,11 +1,8 @@
-// Bai toan cai ba lo 1
-// Moi do vat co so luong khong han che
-// Du lieu cho trong file QHD_CaiBalo.INP
-// Giai bai toan bang thuat toan QUY HOACH DONG
-
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+
+#define debug 0
 
 typedef struct
 {
@@ -13,35 +10,35 @@ typedef struct
 	int TL, GT, PA;
 } DoVat;
 
-typedef int bang[50][100];// Ba lo co trong luong toi da 99 va co toi da 50 do vat
+typedef int bang[50][100];
 
 DoVat *ReadFromFile(int *W, int *n)
 {
-    FILE *f;
-    f = fopen("caibalo1.txt", "r");
-    fscanf(f, "%d",W); // Xac dinh trong luong Ba lo
-	DoVat *dsdv;
-	dsdv=(DoVat*)malloc(sizeof(DoVat));
-	int i=0;
- 	while (!feof(f))
-	{
+     FILE *f;
+     f = fopen("caibalo1.txt", "r");
+     fscanf(f, "%d",W);
+	 DoVat *dsdv;
+	 dsdv=(DoVat*)malloc(sizeof(DoVat));
+	 int i=0;
+ 	 while (!feof(f))
+	  {
 	   fscanf(f, "%d%d%[^\n]",&dsdv[i].TL,&dsdv[i].GT,&dsdv[i].TenDV);
 	   dsdv[i].PA=0;
 	   i++;
 	   dsdv=(DoVat*)realloc(dsdv,sizeof(DoVat)*(i+1));  
 	 }
-	*n=i;
-    fclose(f);
-    return dsdv;
+	 *n=i;
+     fclose(f);
+     return dsdv;
 }
 
 void InDSDV(DoVat *dsdv ,int n, int W)
 {
 	int i, TongTL=0, TongGT=0;
-	printf("\nPhuong an Cai Ba lo 1 dung thuat toan \"QUY HOACH DONG\" nhu sau:\n");
-	printf("|---|------------------|----------|---------|-----------|\n");
-	printf("|STT|     Ten Do Vat   | T Luong  | Gia Tri | Phuong an |\n");
-	printf("|---|------------------|----------|---------|-----------|\n");
+	printf("\nPhuong an Cai Ba lo 1 dung thuat toan QUY HOACH DONG nhu sau:\n");
+		printf("|---|------------------|----------|---------|-----------|\n");
+		printf("|STT|     Ten Do Vat   | T Luong  | Gia Tri | Phuong an |\n");
+		printf("|---|------------------|----------|---------|-----------|\n");
 	for(i=0;i<n;i++)
 	{
 		printf("|%2d |%-18s|%5d     |%5d    |%6d     |\n",
@@ -57,31 +54,29 @@ void InDSDV(DoVat *dsdv ,int n, int W)
 
 void TaoBang(DoVat *dsdv,int n,int W, bang F,bang X)
 {
-	int xk, yk, k;
-	int FMax, XMax, V;
+ 	int xk, yk, k;
+ 	int FMax, XMax, V;
 	
-	// Dien dong dau tien cua hai bang
-	for(V= 0; V<=W; V++)
+ 	for(V= 0; V<=W; V++)
 	{
-		X[0][V] = V/dsdv[0].TL;
-		F[0][V] = X[0][V] * dsdv[0].GT;
-	}
+ 		X[0][V] = V/dsdv[0].TL;
+ 		F[0][V] = X[0][V] * dsdv[0].GT;
+ 	}
 	
-	// ?ien c?c d?ng c?n lai
-	for(k= 1; k<n; k++)
-		for(V=0; V<=W; V++)
-		{
-			FMax = F[k-1][V] ;
-			XMax = 0;
-			yk = V/dsdv[k].TL;
-			for(xk = 1; xk<=yk; xk++)
-			if(F[k-1][V-xk*dsdv[k].TL]+xk*dsdv[k].GT>FMax)
-			{
-				FMax=F[k-1][V-xk*dsdv[k].TL]+xk*dsdv[k].GT;
-				XMax= xk;
-			}
-			F[k][V] = FMax;
-			X[k][V] = XMax;
+	 for(k= 1; k<n; k++)
+		 for(V=0; V<=W; V++)
+		 {
+			 FMax = F[k-1][V] ;
+			 XMax = 0;
+			 yk = V/dsdv[k].TL;
+			 for(xk = 1; xk<=yk; xk++)
+			 	if(F[k-1][V-xk*dsdv[k].TL]+xk*dsdv[k].GT>FMax)
+				 {
+			 		FMax=F[k-1][V-xk*dsdv[k].TL]+xk*dsdv[k].GT;
+			 		XMax= xk;
+			 	}
+			 F[k][V] = FMax;
+			 X[k][V] = XMax;
 		}
 }
 
@@ -91,7 +86,7 @@ void InBang(int n, int W, bang F, bang X)
 	for(k=0; k<n; k++)
 	{
 		for(V=0; V<=W; V++)
-			printf("|%4d%2d",F[k][V], X[k][V]);
+		 	printf("|%4d%2d",F[k][V], X[k][V]);
 		printf("\n");
 	}
 }
@@ -102,8 +97,8 @@ void TraBang(DoVat *dsdv, int n, int W, bang X)
 	V = W;
 	for(k= n-1; k>=0; k--)
 	{
-		dsdv[k].PA = X[k][V];
-		V = V - X[k][V] * dsdv[k].TL;
+	 	dsdv[k].PA = X[k][V];
+	 	V = V - X[k][V] * dsdv[k].TL;
 	}
 }
 
@@ -115,7 +110,7 @@ int main()
 	dsdv=ReadFromFile(&W, &n);
 	
 	TaoBang(dsdv,n,W,F,X);
-	InBang(n,W,F,X);
+	if(debug) InBang(n,W,F,X);
 	printf("\n");
 	TraBang(dsdv,n,W,X);
 	InDSDV(dsdv,n,W);
