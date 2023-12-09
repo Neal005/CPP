641f5a37604f6d0570cadd1f078f46dee8c18dc8
Neal Nguyen <hluc2003@gmail.com>
2023-12-09 22:29:37 +0700

    update function

--- Neal/Function Map/MapRePa.cpp
+++ Neal/Function Map/MapRePa.cpp
@@ -5,7 +5,7 @@
 
 float mapRePa(float x,float head1,float tail1,float head2,float tail2)
 {
-	if(head1>=tail1||head2>=tail2) printf("Error!");
+	if(head1>=tail1||head2>=tail2||x>tail1||x<head1||x<0||head1<0||tail1<0||head2<0||tail2<0) printf("Error!");
 	else
 	{
 		float temp1=tail1,temp2=tail2,solve;
@@ -31,13 +31,16 @@ int main()
 	printf("Enter head 1, tail 1, head 2, tail 2:\n");
 	scanf("%f%f%f%f",&head1,&tail1,&head2,&tail2);
 	solve=mapRePa(x,head1,tail1,head2,tail2);
-	if(debug) printf("\n*Solve = %.5f",solve);
-	else
+	if(solve!=-1)
 	{
-		f = fopen("MapRePa.txt", "w");
-		fprintf(f,"Input: %.5f\n(%.5f-%.5f)->(%.5f-%.5f)\n\t**Solve = %.5f**",x,head1,tail1,head2,tail2,solve);
-		fclose(f);
+    	if(debug) printf("\n*Solve = %.5f",solve);
+    	else
+    	{
+    		f = fopen("MapRePa.txt", "w");
+    		fprintf(f,"Input: %.5f\n(%.5f-%.5f)->(%.5f-%.5f)\n\t**Solve = %.5f**",x,head1,tail1,head2,tail2,solve);
+    		fclose(f);
+    	}
+	    if(debug) Sleep(5000);
 	}
-	if(debug) Sleep(5000);
 	return 0;
 }
